8c3b6afd82f0287e7a66078660063319576708ee
abdohantash <abdohantash9999@gmail.com>
2024-07-23 16:50:57 +0300

    edit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,33 +2,55 @@ $(document).ready(function () {
   $(".preloader").addClass("hide");
 });
 
-$(document).ready(function() {
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 60) {
-          $('.scroll-top').addClass('active');
-      } else {
-          $('.scroll-top').removeClass('active');
-      }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 60) {
+      $(".scroll-top").addClass("active");
+    } else {
+      $(".scroll-top").removeClass("active");
+    }
   });
 
-  $('.scroll-top').click(function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: 0 }, '300');
+  $(".scroll-top").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 500) {
+      $(".buy-veiw-theme").addClass("active");
+    } else {
+      $(".buy-veiw-theme").removeClass("active");
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scrollPosition = $(window).scrollTop();
+    var contactUsPosition = $('#contact-us').offset().top - $(window).height() / 2;
+    if (scrollPosition >= contactUsPosition) {
+      $('.call-whatsapp').addClass('hide');
+    } else {
+      $('.call-whatsapp').removeClass('hide');
+    }
   });
 });
 
 const swiper_testimonial = new Swiper(".swiper-testimonial ", {
   slidesPerView: 1,
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
-  }
+  },
 });
 
 function setCurrentYear() {
@@ -38,5 +60,5 @@ function setCurrentYear() {
 window.onload = setCurrentYear;
 
 AOS.init({
-  once: true
+  once: true,
 });
